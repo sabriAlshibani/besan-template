5071fdfa64d70cbc0221283db9fe7010275e079e
sabriAlshibani <sbryalshybany1@gmail.com>
2025-01-31 11:18:01 +0300

    push some details

--- main.js
+++ main.js
@@ -13,3 +13,31 @@ document.querySelector(".toggle-menu").addEventListener('click', function() {
 //         })
 //     })
 // })
+let items = document.querySelector(".items");
+let item = document.querySelectorAll(".item")[0];
+console.log(item)
+let visibleItems = 2;
+let itemWidth = item.offsetWidth;
+if (itemWidth >= 50) {
+ visibleItems = 1;
+}
+let scrollAmount = 0;
+let maxScroll = (items.children.length - visibleItems) * itemWidth;
+
+function left() {
+    scrollAmount -= itemWidth * visibleItems;
+    console.log(scrollAmount)
+    if (scrollAmount < 0) {
+        scrollAmount = 0;
+    }
+    items.style.transform = `translateX(-${scrollAmount}px)`;
+}
+
+function right() {
+    scrollAmount += itemWidth * visibleItems;
+    console.log(scrollAmount)
+    if (scrollAmount > maxScroll) {
+        scrollAmount = maxScroll;
+    }
+    items.style.transform = `translateX(-${scrollAmount}px)`;
+}
